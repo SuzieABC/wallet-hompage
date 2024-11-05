9756977e8f349f9e05282113cc080db622a0770a
SuzieABC <suzie@ahnlab.io>
2024-11-05 13:04:34 +0900

    feat: add cloudfront domain to Next.js image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {  images: {
+    domains: ['d14g6kounxs3ku.cloudfront.net']
+  }};
 
 export default nextConfig;
