71f44d94ecd6f7882e35fe4607063c724e019d1f
SuzieABC <suzie@ahnlab.io>
2024-11-11 11:17:21 +0900

    chore: amend middleware setting for routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,20 +4,6 @@ import { fallbackLng, locales } from "@/utils/localization/settings";
 export function middleware(request: NextRequest) {  
   const pathname = request.nextUrl.pathname;
 
-  // Paths to redirect to the root URL
-  const pathsToRootRedirect = [`/${fallbackLng}`, '/products'];
-
-  // Redirect to root if the current pathname matches any path
-  if (pathsToRootRedirect.includes(pathname) || pathname === `/${fallbackLng}/`) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  // Redirect /en/products to /en
-  if (locales.some(locale => pathname === `/${locale}/products`)) {
-    const locale = pathname.split('/')[1];
-    return NextResponse.redirect(new URL(`/${locale}`, request.url));
-  }
-
   // Check if the default locale is in the pathname
   if (pathname.startsWith(`/${fallbackLng}`)) {
     return NextResponse.redirect(
