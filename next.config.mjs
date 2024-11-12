06b261601fbad82c9f9d5cc317f6c71d3ec73d30
SuzieABC <suzie@ahnlab.io>
2024-11-12 09:49:41 +0900

    chore: amend image config due to error message

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  images: {
-    domains: ['d14g6kounxs3ku.cloudfront.net']
-  }};
+const nextConfig = {  
+  images: {
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd14g6kounxs3ku.cloudfront.net',
+      },
+    ],
+  }
+
+};
 
 export default nextConfig;
