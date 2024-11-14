4577c19a167e18c2594a2b6cd52ae7b4b86cc2ed
SuzieABC <suzie@ahnlab.io>
2024-11-14 15:48:08 +0900

    chore: delete unnecessary codes

--- src/components/main/FourthIntro.tsx
+++ src/components/main/FourthIntro.tsx
@@ -13,33 +13,16 @@ import oasysIcon from "@/assets/icons/main/fourthIntro/oasys.png";
 import fncyIcon from "@/assets/icons/main/fourthIntro/fncy.png";
 import xLayerIcon from "@/assets/icons/main/fourthIntro/x_layer.png";
 import scrollIcon from "@/assets/icons/main/fourthIntro/scroll.png";
-import arrowDownIcon from "@/assets/icons/main/arrow_down_icon.png";
 import useWindowWidth from "@/utils/hooks/useWindowWidth";
 import dappsImage from "@/assets/images/main/dapp_image.png";
 import dappsMobileImage from "@/assets/images/main/dapp_mobile_image.png";
 
-interface CarouselItem {
-  title: string;
-  description: string;
-  imageSrc: string;
-  tags: string[];
-}
-
-interface DappItem {
-  dapp_title_ko: string;
-  dapp_description_ko: string;
-  logo_url_ko: string;
-  tags_ko: string[];
-}
-
 export default function FourthIntro({
-  datas,
   intro_4_title,
   intro_4_content,
   card1_title,
   card2_title,
 }: {
-  datas: DappItem[];
   intro_4_title: string;
   intro_4_content: string;
   card1_title: string;
@@ -51,11 +34,6 @@ export default function FourthIntro({
   const isTablet = windowWidth >= 700 && windowWidth < 1024;
   const isDesktop = windowWidth >= 1024;
 
-  const [visibleItems, setVisibleItems] = useState(8);
-
-  const handleShowMore = () => {
-    setVisibleItems((prev) => Math.min(prev + 8, datas.length));
-  };
   const coins = [
     { name: "Bitcoin", icon: bitcoinIcon },
     { name: "Ethereum", icon: ethereumIcon },
@@ -69,13 +47,6 @@ export default function FourthIntro({
     { name: "X Layer", icon: xLayerIcon },
     { name: "Scroll", icon: scrollIcon },
   ];
-
-  const items: CarouselItem[] = datas.map((data: DappItem) => ({
-    title: data.dapp_title_ko,
-    description: data.dapp_description_ko,
-    imageSrc: data.logo_url_ko,
-    tags: data.tags_ko,
-  }));
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -198,49 +169,6 @@ export default function FourthIntro({
                 src={isMobile ? dappsMobileImage : dappsImage}
                 alt={"Dapp Image"}
               />
-              {/* <div className="flex flex-wrap justify-center items-center gap-2">
-                {items.map((item) => (
-                  <div
-                    className="grow shrink basis-0 px-3 py-5 bg-[#ececec] rounded-xl flex-col justify-start items-center gap-3 inline-flex"
-                    key={item.title}
-                  >
-                    <div className="w-[132px] h-10 relative flex justify-center items-center">
-                      <div className="w-[37.78px] h-[37.78px] rounded-[22.22px]">
-                        <Image
-                          src={item.imageSrc}
-                          alt={item.title}
-                          width={41.56}
-                          height={41.56}
-                          className="rounded-[22.22px]"
-                        />
-                      </div>
-                    </div>
-                    <div className="self-stretch text-center text-black text-base font-pretendardSemibold">
-                      {item.title}
-                    </div>
-                    <div className="self-stretch h-[38px] flex-col justify-start items-start gap-1 flex">
-                      {item.tags.map((tag) => (
-                        <div
-                          key={tag}
-                          className="self-stretch text-center text-[#5c6070] text-sm font-pretendardMedium"
-                        >
-                          #{tag}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ))}
-                {visibleItems < items.length && (
-                  <div onClick={handleShowMore} className="cursor-pointer mt-4">
-                    <Image
-                      src={arrowDownIcon}
-                      alt="arrowRight"
-                      width={24}
-                      height={24}
-                    />
-                  </div>
-                )}
-              </div> */}
             </div>
           </div>
         </div>
