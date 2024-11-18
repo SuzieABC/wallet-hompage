fcafd82dc3f0a33c9467e088bd1696957e0a2d0f
SuzieABC <suzie@ahnlab.io>
2024-11-18 17:44:29 +0900

    chore: change name of button file

--- src/components/elements/DeepLinkButton.tsx
+++ src/components/elements/DeepLinkButton.tsx
@@ -2,11 +2,13 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import arrowRight from "@/assets/icons/main/top_arrow_right_icon.svg";
-import arrowRightWhite from "@/assets/icons/main/top_arrow_right_white_icon.svg";
+import arrowRight from "@/assets/icons/main/top/top_arrow_right_icon.svg";
+import arrowRightWhite from "@/assets/icons/main/top/top_arrow_right_white_icon.svg";
 import useWindowWidth from "@/utils/hooks/useWindowWidth";
+import { useParams } from "next/navigation";
+import type { LocaleTypes } from "@/utils/localization/settings";
 
-export default function Button() {
+export default function DeepLinkButton() {
   const windowWidth = useWindowWidth();
 
   const isMobile = windowWidth < 700;
@@ -18,39 +20,45 @@ export default function Button() {
   const [storeUrl, setStoreUrl] = useState<string | null>(null);
   const [platform, setPlatform] = useState<string>("");
 
+  const locale = useParams()?.locale as LocaleTypes;
+
   useEffect(() => {
     // 클라이언트 측에서 navigator 객체를 사용하여 사용자 기기를 감지
     const userAgent =
       typeof navigator !== "undefined" ? navigator.userAgent : "";
 
     const isAndroid = /android/i.test(userAgent || "");
-    const isIOS = /iPhone|iPad|iPod|Macintosh|Mac OS X/i.test(userAgent || "");
+    // const isIOS = /iPhone|iPad|iPod|Macintosh|Mac OS X/i.test(userAgent || "");
+    const isIOS = /iPhone|iPad|iPod/i.test(userAgent || "");
+
     const isChrome = /chrome|chromium|crios/i.test(userAgent || "");
 
     if (isAndroid) {
       setStoreUrl(
-        "https://play.google.com/store/apps/details?id=io.myabcwallet.mpc"
+        `https://play.google.com/store/apps/details?id=io.myabcwallet.mpc&hl=${locale}`
       );
       setPlatform("Android");
     } else if (isIOS) {
       setStoreUrl(
-        "https://apps.apple.com/us/app/abc-wallet-safe-web3-wallet/id1642837445"
+        `https://apps.apple.com/${locale}/app/abc-wallet-safe-web3-wallet/id1642837445`
       );
       setPlatform("iOS");
     } else if (isChrome) {
       setPlatform("Chrome");
       setStoreUrl(
-        "https://play.google.com/store/apps/details?id=io.myabcwallet.mpc"
+        `https://chromewebstore.google.com/detail/abc-wallet/mlhakagmgkmonhdonhkpjeebfphligng?hl=${
+          locale === "jp" ? "ja" : locale
+        }`
       );
     } else {
       setPlatform("Unknown");
       setStoreUrl(null); // Android나 iOS가 아닌 경우 URL은 null로 유지
     }
-  }, []);
+  }, [locale]);
 
   const handleDownloadClick = () => {
     if (storeUrl) {
-      window.location.href = storeUrl;
+      window.open(storeUrl, "_blank"); // Open in a new tab
     } else {
       alert(
         `${platform} 기기에서는 앱 다운로드가 지원되지 않거나, 다른 링크로 연결될 수 있습니다.`
@@ -71,9 +79,9 @@ export default function Button() {
         isMobile
           ? "mb-[40px] mt-[24px] py-3"
           : isTablet
-          ? "mb-[17.42px] mt-[20px] py-3"
+          ? "mb-[20px] mt-[20px] py-3"
           : isDesktop
-          ? "mt-[21px] mb-[19px] py-3 px-5"
+          ? "mt-[21px] mb-[19.93px] py-3 px-5"
           : "mt-[48px] py-[20px] px-[32px]"
       }`}
       onClick={handleDownloadClick}
