fdb9339a239eeb929f40f2078627dcb25d5590e0
SuzieABC <suzie@ahnlab.io>
2024-11-19 16:23:41 +0900

    refactor: remove isLoading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@
 
 import { useState, useEffect } from "react";
 import { useParams, usePathname } from "next/navigation";
+import { useTranslation } from "@/utils/localization/client";
 import Link from "next/link";
 import Image from "next/image";
 import ChangeLocale from "./ChangeLocale";
-import { useTranslation } from "@/utils/localization/client";
 import type { LocaleTypes } from "@/utils/localization/settings";
-import logo from "@/assets/images/header/bi_color.png";
 import useWindowWidth from "@/utils/hooks/useWindowWidth";
+import logo from "@/assets/images/header/bi_color.png";
 
 export default function Header() {
   const windowWidth = useWindowWidth();
@@ -35,73 +35,62 @@ export default function Header() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
   return (
-    <>
-      {isLoading && (
-        <div
-          className={`z-50 fixed top-0 left-1/2 transform -translate-x-1/2 ${
-            isDesktop || isLargeDesktop ? "py-[12px]" : "py-[7px]"
-          } w-full ${isScrolled ? "bg-white" : "bg-transparent"}`}
-        >
-          <header
-            className={`max-w-[1440px] mx-auto w-full flex flex-col justify-center    ${
-              isMobile || isTablet ? "pl-[20px] pr-[16px]" : "px-[40px]"
-            } `}
+    <div
+      style={{ visibility: windowWidth ? "visible" : "hidden" }}
+      className={`z-50 fixed top-0 left-1/2 transform -translate-x-1/2 ${
+        isDesktop || isLargeDesktop ? "py-[12px]" : "py-[7px]"
+      } w-full ${isScrolled ? "bg-white" : "bg-transparent"}`}
+    >
+      <header
+        className={`max-w-[1440px] mx-auto w-full flex flex-col justify-center ${
+          isMobile || isTablet ? "pl-[20px] pr-[16px]" : "px-[40px]"
+        } `}
+      >
+        <div className="flex flex-row w-full justify-between items-center">
+          {/* Logo */}
+          <Link
+            className="flex flex-row items-center cursor-pointer"
+            href={`/${locale}/`}
           >
-            <div className="flex flex-row w-full justify-between items-center">
-              {/* Logo */}
-              <Link
-                className="flex flex-row items-center cursor-pointer"
-                href={`/${locale}/`}
-              >
-                <Image
-                  src={logo}
-                  alt="logo"
-                  priority
-                  width={isDesktop || isLargeDesktop ? 140 : 119}
-                  height={isDesktop || isLargeDesktop ? 20 : 16.44}
-                />
-              </Link>
+            <Image
+              src={logo}
+              alt="logo"
+              priority
+              width={isDesktop || isLargeDesktop ? 140 : 119}
+              height={isDesktop || isLargeDesktop ? 20 : 16.44}
+            />
+          </Link>
 
-              {/* Right Section */}
-              <div className="flex">
-                {/* Desktop Navigation */}
+          {/* Right Section */}
+          <div className="flex">
+            {/* Desktop Navigation */}
 
-                <nav
-                  className={`flex flex-row justify-center items-center ${
-                    isDesktop || isLargeDesktop ? "gap-2.5" : ""
+            <nav
+              className={`flex flex-row justify-center items-center ${
+                isDesktop || isLargeDesktop ? "gap-2.5" : ""
+              }`}
+            >
+              {items.map((item) => (
+                <Link
+                  key={item}
+                  href={`/${locale}/${item}`}
+                  className={`p-[16px] ${
+                    isDesktop || isLargeDesktop ? "text-[18px]" : "text-[15px]"
+                  } leading-none ${
+                    pathName.includes(item)
+                      ? "text-[#000000] font-archivoSemibold "
+                      : "text-[#454854] font-archivoMedium "
                   }`}
                 >
-                  {items.map((item) => (
-                    <Link
-                      key={item}
-                      href={`/${locale}/${item}`}
-                      className={`p-[16px] ${
-                        isDesktop || isLargeDesktop
-                          ? "text-[18px]"
-                          : "text-[15px]"
-                      } leading-none ${
-                        pathName.includes(item)
-                          ? "text-[#000000] font-archivoSemibold "
-                          : "text-[#454854] font-archivoMedium "
-                      }`}
-                    >
-                      {t(`${item}`)}
-                    </Link>
-                  ))}
-                  <ChangeLocale windowWidth={windowWidth} />
-                </nav>
-              </div>
-            </div>
-          </header>
+                  {t(`${item}`)}
+                </Link>
+              ))}
+              <ChangeLocale windowWidth={windowWidth} />
+            </nav>
+          </div>
         </div>
-      )}
-    </>
+      </header>
+    </div>
   );
 }
