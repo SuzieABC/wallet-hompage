15270b32bc2f0809524c9bf58ab4cafa891d7eab
SuzieABC <suzie@ahnlab.io>
2024-11-21 11:23:33 +0900

    refactor: refactored code for simplicity

--- src/app/[locale]/support/page.tsx
+++ src/app/[locale]/support/page.tsx
@@ -5,55 +5,56 @@ import Notice from "@/components/support/Notice";
 import FAQ from "@/components/support/FAQ";
 import Enquiry from "@/components/support/Enquriy";
 
+// 공통 API 호출 함수
+async function fetchData(url: string, errorMessage: string) {
+  const response = await fetch(url, { cache: "no-store" }); // 항상 최신 데이터
+  if (!response.ok) {
+    throw new Error(errorMessage);
+  }
+  return response.json();
+}
+
 export default async function Page({
   params: { locale },
 }: {
   params: { locale: LocaleTypes };
 }) {
-  // Make sure to use the correct namespace here.
   const { t } = await createTranslation(locale, "support");
 
-  const resNotice = await fetch(
-    `https://dev-api.id.myabcwallet.com/query/v2/notice/items?service=wallet-homepage&sort=create-time&order=desc&language=${locale}&offset=0&limit=999`,
-    {
-      cache: "no-store", // API 데이터가 항상 최신이도록 설정
-    }
-  );
-
-  if (!resNotice.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const noticeData = await resNotice.json();
-
-  const resFAQ = await fetch(
-    `https://dev-api.id.myabcwallet.com/query/v2/faq/items?service=wallet-homepage&category=all&sort=update-time&order=desc&language=${locale}&offset=0&limit=999`,
-    {
-      cache: "no-store", // API 데이터가 항상 최신이도록 설정
-    }
-  );
-
-  if (!resFAQ.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const faqData = await resFAQ.json();
+  // API 요청
+  const [noticeData, faqData] = await Promise.all([
+    fetchData(
+      `https://dev-api.id.myabcwallet.com/query/v2/notice/items?service=wallet-homepage&sort=create-time&order=desc&language=${locale}&offset=0&limit=999`,
+      "Failed to fetch notice data"
+    ),
+    fetchData(
+      `https://dev-api.id.myabcwallet.com/query/v2/faq/items?service=wallet-homepage&category=all&sort=update-time&order=desc&language=${locale}&offset=0&limit=999`,
+      "Failed to fetch FAQ data"
+    ),
+  ]);
 
   return (
     <div className="flex flex-col justify-center items-center">
+      {/* 상단 support 페이지 제목 */}
       <SupportTop title={t("title")} />
+
+      {/* 공지 사항 */}
       <Notice
-        datas={noticeData["rows"]}
+        datas={noticeData.rows}
         notice_title={t("notice.title")}
         button={t("view_all")}
         locale={locale}
       />
+
+      {/* FAQ 섹션 */}
       <FAQ
-        datas={faqData["rows"]}
+        datas={faqData.rows}
         faq_title={t("faq.title")}
         button={t("view_all")}
         locale={locale}
       />
+
+      {/* 문의하기 섹션 */}
       <Enquiry
         title_1={t("enquiry.title_1")}
         title_2={t("enquiry.title_2")}
