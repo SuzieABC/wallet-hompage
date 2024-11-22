a3b328ebe2e2789b9d0ec5a17b0a2bb1d7112562
SuzieABC <suzie@ahnlab.io>
2024-11-22 09:55:04 +0900

    chore: change top image and styles

--- src/components/main/Top.tsx
+++ src/components/main/Top.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import useWindowWidth from "@/utils/hooks/useWindowWidth";
 import DeepLinkButton from "@/components/elements/DeepLinkButton";
+import { usePathname } from "next/navigation";
 import backgroundMobile from "@/assets/images/main/top/top_background_mobile_image.png";
 import backgroundTablet from "@/assets/images/main/top/top_background_tablet_image.png";
 import backgroundDesktop from "@/assets/images/main/top/top_background_desktop_image.png";
@@ -10,6 +11,7 @@ import backgroundLargeDesktop from "@/assets/images/main/top/top_background_larg
 import mobile from "@/assets/images/main/top/top_mobile_image.png";
 import tablet from "@/assets/images/main/top/top_tablet_image.png";
 import desktop from "@/assets/images/main/top/top_desktop_image.png";
+import largeDesktop from "@/assets/images/main/top/top_large_desktop_image.png";
 
 interface TopProps {
   title: string;
@@ -30,13 +32,16 @@ export default function Top({
   const isMobile = windowWidth < 700;
   const isTablet = windowWidth >= 700 && windowWidth < 1024;
   const isDesktop = windowWidth >= 1024 && windowWidth < 1440;
-  const isLargeDesktop = windowWidth >= 1440;
+  const isLargeDesktop = windowWidth >= 1440 && windowWidth < 1919;
+  const isExtraLargeDesktop = windowWidth >= 1920;
+
+  const pathName = usePathname();
 
   return (
     <div
       id="download"
       className={`scroll-smooth flex items-center ${
-        isLargeDesktop ? "w-[100vw]" : "w-full"
+        isLargeDesktop || isExtraLargeDesktop ? "w-[100vw]" : "w-full"
       } h-lvh`}
       style={{
         backgroundImage: `url(${
@@ -72,17 +77,25 @@ export default function Top({
         >
           <div
             className={`flex flex-col ${
-              isLargeDesktop ? "items-start w-[494px]" : "items-center"
+              isLargeDesktop || isExtraLargeDesktop
+                ? "items-start w-[494px]"
+                : "items-center"
             } tracking-[-0.19px]`}
           >
             <span
-              className="
-                  text-center text-black font-archivoSemibold leading-none
-                  mobile:text-[38px]
-                  tablet:text-[50px]
-                  desktop:text-[64px]
-                  largeDesktop:text-[82px]
-                "
+              className={`text-center text-black font-archivoSemibold largeDesktop:font-archivoBold
+                  ${
+                    isMobile
+                      ? "text-[38px]"
+                      : isTablet
+                      ? "text-[50px]"
+                      : isDesktop
+                      ? "text-[64px]"
+                      : isLargeDesktop
+                      ? "text-[74px]"
+                      : "text-[80px] leading-[83.20px]"
+                  }
+                  `}
             >
               {title}
             </span>
@@ -101,32 +114,56 @@ export default function Top({
                     ? "text-[50px] font-pretendardSemibold"
                     : isDesktop
                     ? "text-[64px] font-pretendardSemibold"
-                    : "text-[86px] font-pretendardSemibold leading-[100.04px]"
+                    : isLargeDesktop
+                    ? "text-[78px] font-pretendardSemibold leading-[100.04px]"
+                    : `${
+                        pathName === "/ja"
+                          ? "leading-[98.4px]"
+                          : pathName === "/ko"
+                          ? "leading-[95.12px]"
+                          : "leading-[97.44px]"
+                      } font-pretendardSemibold leading-[100.04px] ${
+                        pathName === "/en" ? "text-[84px]" : "text-[82px]"
+                      }`
                 }`}
               >
                 {subtitle_1}&nbsp;
               </span>
-              {isLargeDesktop && <br />}
+              {(isLargeDesktop || isExtraLargeDesktop) && <br />}
               <span
-                className="text-[#6d23ef] font-archivoSemibold leading-none text-center 
+                className={`text-[#6d23ef] font-archivoSemibold text-center 
                     mobile:text-[38px] 
                     tablet:text-[50px] 
-                    desktop:text-[64px] 
-                    largeDesktop:text-[86px]"
+                    desktop:text-[64px] ${
+                      pathName === "/ja" && isExtraLargeDesktop
+                        ? "leading-[92.40px] text-[84px]"
+                        : (pathName === "/en" || pathName === "/ko") &&
+                          isExtraLargeDesktop &&
+                          "leading-[97.44px] text-[84px]"
+                    }`}
               >
                 {subtitle_2}&nbsp;
               </span>
+              {pathName === "/ja" && <br />}
               <span
-                className="text-[#6d23ef] leading-none whitespace-pre
+                className={`text-[#6d23ef] leading-none whitespace-pre
                   mobile:text-[38px] mobile:font-pretendardBold
                   tablet:text-[50px] tablet:font-pretendardSemibold
                   desktop:text-[64px] desktop:font-pretendardSemibold
-                  largeDesktop:text-[86px] largeDesktop:font-pretendardSemibold"
+                  largeDesktop:font-pretendardSemibold ${
+                    pathName === "/ja" && isExtraLargeDesktop
+                      ? "leading-[92.40px] text-[82px]"
+                      : pathName === "/ko" && isExtraLargeDesktop
+                      ? "leading-[97.44px] text-[82px]"
+                      : pathName === "/en" &&
+                        isExtraLargeDesktop &&
+                        "leading-[97.44px] text-[84px]"
+                  }`}
               >
                 {subtitle_3}
               </span>
             </div>
-            {isLargeDesktop && (
+            {(isLargeDesktop || isExtraLargeDesktop) && (
               <p className="pt-[16px] font-pretendardRegular leading-[30px] text-xl">
                 {detail}
               </p>
@@ -136,11 +173,28 @@ export default function Top({
         </div>
 
         <Image
-          src={isMobile ? mobile : isTablet ? tablet : desktop}
+          src={
+            isMobile
+              ? mobile
+              : isTablet
+              ? tablet
+              : isDesktop
+              ? desktop
+              : largeDesktop
+          }
           alt="mobile"
           priority
-          className={`h-auto mobile:w-[320px] tablet:w-[530px]
-              desktop:w-[649.53px] largeDesktop:w-[772px]`}
+          className={`h-auto ${
+            isMobile
+              ? "w-[320px]"
+              : isTablet
+              ? "w-[530px]"
+              : isDesktop
+              ? "w-[649.53px]"
+              : isLargeDesktop
+              ? "w-[718px]"
+              : "w-[778px]"
+          }`}
         />
       </div>
     </div>
